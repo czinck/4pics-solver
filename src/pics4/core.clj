1d82759d5be3afa78665507eeda1defd514c0188
Christian <christian@mint>
2013-03-21 17:23:46 -0400

    better approach to printing the solutions

--- src/pics4/core.clj
+++ src/pics4/core.clj
@@ -3,21 +3,24 @@
 
 (defn unordered_hash [word] 
   (let [primes [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]]
-    (apply * (map (fn [i] (nth primes i)) (map (fn [c] (- c 97)) (map int word))))
+    (apply * (map (fn [letter] (get primes (- (int letter) 97))) word))
   )
 )
 
 (defn get_dictionary [n]
-  (let [words (filter (fn [word] (and (re-find #"^[a-z]*$" word) (= (count word) n))) (clojure.string/split-lines (slurp "/home/christian/cracklib-small")))]
+  (let [words (filter (fn [word] (and (= (count word) n) (re-find #"^[a-z]+$" word)))
+      (clojure.string/split-lines (slurp "/home/christian/cracklib-small")))]
     (group-by unordered_hash words)
   )
 )
 
 (defn -main [& args]
   (let [dict (get_dictionary (read-string (second args)))]
-    (doseq [word (combo/combinations (first args) (read-string (second args)))]
-      (if (get dict (unordered_hash word))
-        (println (get dict (unordered_hash word)))
+    (print 
+      (filter (fn [word] (boolean word)) 
+        (map (fn [word] (get dict (unordered_hash word))) 
+          (combo/combinations (first args) (read-string (second args)))
+        )
       )
     )
   )
